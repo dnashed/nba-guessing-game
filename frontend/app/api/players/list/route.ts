3083096ba9fb828ff00321d69cb7bec5537ba1b4
David N. <dnashed9@gmail.com>
2025-12-28 15:12:53 -0500

    update

--- frontend/app/api/players/list/route.ts
+++ frontend/app/api/players/list/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY 
+const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseKey = process.env.SUPABASE_KEY!
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
